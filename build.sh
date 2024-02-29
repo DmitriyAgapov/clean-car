9ebcc61ad3b6c80f199b954b859fb870c530c3de
Alexandr Birukov <birukov.system@gmail.com>
2024-02-29 12:47:51 +0300

    chown

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Building React output
 sudo yarn install
 sudo yarn run build
+sudo chown -R gitlab-runner:gitlab-runner build/ node_modules/ 
 
 echo "Deploying to server..."
 echo "Finished copying the build files"
