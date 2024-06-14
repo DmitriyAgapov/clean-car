68bb141f54322a5fadaf7cab40e43f58f8a96187
ryabov <ryabovser@gmail.com>
2024-06-14 21:09:22 +0500

    fix названия шаблона

--- src/routes/users/usersCreateAction.tsx
+++ src/routes/users/usersCreateAction.tsx
@@ -104,7 +104,7 @@ export default function UsersPageCreateAction() {
             />
             {store.appStore.appType === "admin" && <><Button
               text={'Скачать шаблон'}
-              href={'/Шаблон_пользователи.xlsx'}
+              href={'/Шаблон_автомобилей.xlsx'}
               variant={ButtonVariant["accent-outline"]}
               // action={() => navigate('/account/users/create')}
               className={'inline-flex mr-5'}
