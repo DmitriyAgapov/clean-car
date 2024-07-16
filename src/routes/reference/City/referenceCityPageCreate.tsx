3c6390f30ae6d0217822ee533564fdcebccbd30b
DmitriyAgapov <93589129+DmitriyAgapov@users.noreply.github.com>
2024-07-16 22:17:49 +0500

    Справочники не обновляются теперь

--- src/routes/reference/City/referenceCityPageCreate.tsx
+++ src/routes/reference/City/referenceCityPageCreate.tsx
@@ -4,13 +4,10 @@ import Panel, { PanelVariant } from 'components/common/layout/Panel/Panel'
 import Heading, { HeadingColor, HeadingVariant } from 'components/common/ui/Heading/Heading'
 import Button, {  ButtonVariant } from "components/common/ui/Button/Button";
 import { useStore } from 'stores/store'
-import { Navigate, useLoaderData, useLocation, useNavigate, useParams } from "react-router-dom";
+import { Navigate, useLocation, useNavigate, useParams } from "react-router-dom";
 import { SvgBackArrow } from 'components/common/ui/Icon'
 import { PermissionNames } from "stores/permissionStore";
-import FormCreateCarBrand from "components/Form/FormCreateCarBrand/FormCreateCarBrand";
-import { useMap } from "react-leaflet";
 import { textDataCities } from "routes/reference/City/cities";
-import FormCreateUpdateCarBrand from "components/Form/FormCreateCarBrand/FormCreateUpdateCarBrand";
 import agent from "utils/agent";
 import FormCreateCity from "components/Form/FormCreateCity/FormCreateCity";
 import useSWR from "swr";
@@ -33,12 +30,12 @@ function ReferenceCityPageCreate(props: any) {
           <SvgBackArrow />{textDataCities.createPageBack}</>} className={'flex items-center gap-2 font-medium text-[#606163] hover:text-gray-300 leading-none !mb-4'} action={() => navigate(location.pathname.split('/').slice(0, -1).join('/'))} variant={ButtonVariant.text} />
           <Heading text={!location.pathname.includes('edit') ? textDataCities.createPage : textDataCities.editPage} variant={HeadingVariant.h1} className={'inline-block !mb-0'} color={HeadingColor.accent} /></div></>}>
       </Panel>
-      {!isLoading && <FormCreateCity edit={location.pathname.includes('edit')} {...location.pathname.includes('edit') ? ({
+      <FormCreateCity edit={location.pathname.includes('edit')} {...location.pathname.includes('edit') ? ({
         id: data.id,
         city: data.name,
         timezone: data.timezone,
         is_active: data.is_active ? 'true' : 'false',
-      }) : null}/>}
+      }) : null}/>
       {/* {!location.pathname.includes('edit') ? textDataCars.createPageForm() : textDataCars.editPageForm(props)} */}
 
     </Section>
